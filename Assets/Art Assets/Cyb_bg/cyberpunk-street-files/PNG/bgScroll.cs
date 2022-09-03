25b5655ff2deb89eeea38c12510f3d6a09cd628f
shamblez2k <shalif.shaoul@gmail.com>
2022-09-02 22:19:18 -0400

    looping bg added to branch bg

---
+++ Assets/Art Assets/Cyb_bg/cyberpunk-street-files/PNG/bgScroll.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class bgScroll : MonoBehaviour
+{
+    [Range(-1f,1f)]
+    public float scrollSpeed = 0.5f;
+    private float offset;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
